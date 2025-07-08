934c4ca3ff981ca51bc9f2a40bd97de81f0b8a99
Manthavy <lambopremo@gmail.com>
2025-07-08 15:32:18 +0700

    seven

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,8 +11,8 @@ import { getFirestore } from 'firebase/firestore';
 //   measurementId: "G-5PWFBV7FPW",
 // };
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_YKE,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUT_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
